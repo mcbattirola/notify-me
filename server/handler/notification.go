8956ffa7c0e83a90502833ddd14ed9447cf1b9e4
matheus <mcbattirola@hotmail.com>
2020-11-24 22:35:52 -0300

    Finished POST notification.

--- server/handler/notification.go
+++ server/handler/notification.go
@@ -4,10 +4,33 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/mcbattirola/notify-me/server/models"
 )
 
+type NotificationDTO struct {
+	Title string `json:"title" binding:"required"`
+	Body  string `json:"body" binding:"required"`
+}
+
 func PostNotification(c *gin.Context) {
-	c.JSON(http.StatusOK, gin.H{
-		"data": "ok",
+	var input NotificationDTO
+
+	if err := c.ShouldBindJSON(&input); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	notification, err := models.CreateNotification(models.Notification{
+		Title: input.Title,
+		Body:  input.Body,
+	})
+
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(200, gin.H{
+		"data": notification,
 	})
 }
